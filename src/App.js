b9569383284c7779cf7e371eedcd3a3966aa956c
kh8514 <lostwaykh@nate.com>
2023-10-03 21:55:00 +0900

    dark mode

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       <Header filter={filter} filters={filters} onFilterChange={setFilter} />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
